d324cc05b2143ce4586892effd0ba7e4e8daa8f5
Luke Taylor <lmcjt@outlook.com>
2017-05-04 17:23:05 +0100

    :sparkles: Introduced native module to show alert on iOS

--- src/components/ProfileScreen.js
+++ src/components/ProfileScreen.js
@@ -2,15 +2,25 @@ import React from 'react';
 import {
   Text,
   View,
+  Button
 } from 'react-native';
 
 import Styles from '../themes/Styles';
+import { getAlertFromNative } from '../native/index';
 
 const ProfileScreen = () => (
   <View style={ Styles.container }>
     <Text style={ Styles.welcome }>
       Profile Screen
     </Text>
+    <Button
+      title="Native Alert 1"
+      onPress={ () => getAlertFromNative("This is my first example.") }
+    />
+    <Button
+      title="Native Alert 2"
+      onPress={ () => getAlertFromNative("This is my second example.") }
+    />
   </View>
 );
 
